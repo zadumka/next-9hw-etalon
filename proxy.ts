5822d85554ed68d5376e0fa444d5b78342465e43
zadumka <90329284+zadumka@users.noreply.github.com>
2025-12-29 22:39:49 +0200

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -8,7 +8,7 @@ import { checkSession } from './lib/api/serverApi';
 const privateRoutes = ['/profile', '/notes'];
 const publicRoutes = ['/sign-in', '/sign-up'];
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const cookieStore = await cookies();
   const accessToken = cookieStore.get('accessToken')?.value;
